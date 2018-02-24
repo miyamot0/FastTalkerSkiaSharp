a9f948b8f4c150b234173193cae1e1a9089860ac
miyamot0 <78b2efb6197198b3774066ed9bcca161236c96d0>
2018-02-24 00:39:53 +0000

    Licensing stuff

--- FastTalkerSkiaSharp/FastTalkerSkiaSharp.Android/Controls/ViewCellTransparent.cs
+++ FastTalkerSkiaSharp/FastTalkerSkiaSharp.Android/Controls/ViewCellTransparent.cs
@@ -1,10 +1,33 @@
-﻿using Android.Content;
+﻿/*
+   Copyright February 8, 2016 Shawn Gilroy
+
+   This file is part of Fast Talker
+  
+   Fast Talker is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, version 3.
+
+   Fast Talker is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Fast Talker.  If not, see http://www.gnu.org/licenses/. 
+   </copyright>
+
+   The Fast Talker is a tool to assist clinicans and researchers in the treatment of communication disorders.
+
+   Email: shawn(dot)gilroy(at)temple.edu
+*/
+
+using Android.Content;
 using Android.Views;
 using FastTalkerSkiaSharp.Droid.Controls;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 
-//[assembly: ExportRenderer(typeof(ViewCell), typeof(ViewCellTransparent))]
+[assembly: ExportRenderer(typeof(ViewCell), typeof(ViewCellTransparent))]
 namespace FastTalkerSkiaSharp.Droid.Controls
 {
     public class ViewCellTransparent : ViewCellRenderer
@@ -13,7 +36,6 @@ namespace FastTalkerSkiaSharp.Droid.Controls
         {
             var cell = base.GetCellCore(item, convertView, parent, context);
 
-            /*
             var listView = parent as Android.Widget.ListView;
 
             if (listView != null)
@@ -21,7 +43,6 @@ namespace FastTalkerSkiaSharp.Droid.Controls
                 listView.SetSelector(Android.Resource.Color.Transparent);
                 listView.CacheColorHint = Android.Graphics.Color.Transparent;
             }
-            */
 
             return cell;
         }
--- FastTalkerSkiaSharp/FastTalkerSkiaSharp.Android/Implementations/ImplementationSaveLoad.cs
+++ FastTalkerSkiaSharp/FastTalkerSkiaSharp.Android/Implementations/ImplementationSaveLoad.cs
@@ -1,4 +1,26 @@
-﻿using Xamarin.Forms;
+﻿/*
+   Copyright February 8, 2016 Shawn Gilroy
+
+   This file is part of Fast Talker
+  
+   Fast Talker is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, version 3.
+
+   Fast Talker is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Fast Talker.  If not, see http://www.gnu.org/licenses/. 
+   </copyright>
+
+   The Fast Talker is a tool to assist clinicans and researchers in the treatment of communication disorders.
+
+   Email: shawn(dot)gilroy(at)temple.edu
+*/
+using Xamarin.Forms;
 
 [assembly: Dependency(typeof(FastTalkerSkiaSharp.Droid.Implementations.ImplementationSaveLoad))]
 namespace FastTalkerSkiaSharp.Droid.Implementations
