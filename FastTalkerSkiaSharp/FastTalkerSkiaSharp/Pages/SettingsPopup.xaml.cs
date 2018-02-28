cc80cc6c80f06432c266817046c1b973585cbfb3
miyamot0 <78b2efb6197198b3774066ed9bcca161236c96d0>
2018-02-28 22:21:33 +0000

    [feature] visual settings menu

---
+++ FastTalkerSkiaSharp/FastTalkerSkiaSharp/Pages/SettingsPopup.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using FastTalkerSkiaSharp.ViewModels;
+using Rg.Plugins.Popup.Pages;
+using Xamarin.Forms;
+
+namespace FastTalkerSkiaSharp.Pages
+{
+    public partial class SettingsPopup : PopupPage
+    {
+        public SettingsPopup()
+        {
+            InitializeComponent();
+
+            BindingContext = new PopupUpViewModel()
+            {
+                Padding = new Thickness(100, 100, 100, 100),
+                IsSystemPadding = true
+            };
+        }
+    }
+}
